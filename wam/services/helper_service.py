de1926f923c7417381b4704c629e1312f6a8dc27
sagi-viktor <sagi.viktor1@gmail.com>
2022-11-16 13:17:07 +0100

    chore: add helper service

---
+++ wam/services/helper_service.py
@@ -0,0 +1,13 @@
+
+"""This service helps the table management, with fix and or calculated values"""
+
+USER_ENTERED = "USER_ENTERED"
+
+
+def calc_range():
+    """Table range (table width)
+    :return: String table_range"""
+
+    # Temporarily solution is to provide the whole table width
+    # It works without any error
+    return 'A1:Z1'
