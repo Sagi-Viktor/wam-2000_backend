e70be595aa861d951b2d146ccdbb094403529f4b
sagi-viktor <sagi.viktor1@gmail.com>
2022-11-16 13:16:46 +0100

    chore: add update sheet service

---
+++ wam/services/update_service.py
@@ -0,0 +1,15 @@
+from authentication import authentication
+from googleapiclient import discovery
+
+
+def update(spreadsheet_id, data, range_, value_input_option, insert_data_option):
+    creds = authentication.start()
+    service = discovery.build('sheets', 'v4', credentials=creds)
+    print(f"Updating Spreadsheet, ID: {spreadsheet_id}")
+    request = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id,
+                                                     range=range_, valueInputOption=value_input_option,
+                                                     insertDataOption=insert_data_option,
+                                                     body=data)
+    response = request.execute()
+    print(f"{response['updates']['updatedColumns']} cells updated.")
+    return response
